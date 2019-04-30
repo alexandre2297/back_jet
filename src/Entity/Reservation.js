c6c3b57273b9a63cab1901091989f7584572bff6
Jermin94 <jer5@live.fr>
2019-04-30 16:22:56 +0200

    Add Reservation Class

---
+++ src/Entity/Reservation.js
@@ -0,0 +1,40 @@
+module.exports = class {
+    constructor() {
+        this._id = null;
+        this._start = null;
+        this._end = null;
+    }
+
+
+    get id() {
+        return this._id;
+    }
+
+    set id(value) {
+        this._id = value;
+    }
+
+    get start() {
+        return this._start;
+    }
+
+    set start(value) {
+        this._start = value;
+    }
+
+    get end() {
+        return this._end;
+    }
+
+    set end(value) {
+        this._end = value;
+    }
+
+    toJson() {
+        return {
+            id : this.id,
+            name: this.start,
+            image: this.end
+        }
+    }
+};
